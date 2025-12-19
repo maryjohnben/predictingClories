86076f4e6c5da5f17e180d844335b238639ad2c8
Mary Benjamin <marybenjamin713@gmail.com>
2025-12-19 16:54:06 -0600

    trying to fix git at deployment

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 import { initAuth } from "./profile.js";
 import { initGame } from "./game.js";
 
+console.log("main.js loaded");
+alert("main.js loaded");
+
 document.addEventListener("DOMContentLoaded", () => {
   initAuth();
   initGame();
